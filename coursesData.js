de3b34749ee64c4269cdbcd1e355b2a4f6eb37fa
HaydenMeloche <meloche.hayden@gmail.com>
2018-12-17 23:35:51 -0500

    Sample graphQL demo app

---
+++ coursesData.js
@@ -0,0 +1,26 @@
+[
+    {
+        id: 1,
+        title: 'The Complete Guide to Passing School',
+        author: 'A guy',
+        description: 'Help',
+        topic: 'School',
+        url: 'www.google.ca'
+    },
+    {
+        id: 2,
+        title: 'another title',
+        author: 'Brad',
+        description: 'sdfa',
+        topic: 'Node.js',
+        url: 'asdf'
+    },
+    {
+        id: 3,
+        title: 'Github',
+        author: 'Hayden',
+        description: 'An advanced guide to github',
+        topic: 'Git',
+        url: 'https://www.github.com'
+    }
+]
---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    }
+`);
+var coursesData = require('./coursesData.json');
+
+var getCourse = function(args) { 
+    var id = args.id;
+    return coursesData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+var getCourses = function(args) {
+    if (args.topic) {
+        var topic = args.topic;
+        return coursesData.filter(course => course.topic === topic);
+    } else {
+        return coursesData;
+    }
+}
+var root = {
+    course: getCourse,
+    courses: getCourses
+};
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true //this enables the web UI for graphql
+}));
+
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
